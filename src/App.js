c206b50a6ba5e2e0488859f27a656ba2e42c4802
Gaeta Pamela <odontologia.pamela@gmail.com>
2022-07-22 07:40:16 -0300

    renderizando

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+import React,{Component} from "react";
+import fruta from "./uva.webp"
+export default class app extends Component{
+  state={ 
+    nome:"Pamela",
+    idade:"25",
+    Sua_comida_favorita:"Bife a milanesa",
+    musica:[
+      "Thinking out loud- Ed Sheeran",
+      "Senõrita- Shaw Mendes",
+      "Jorge e Matheus"
+    ]
+  }
+  render(){
+    return(
+      <header> 
+        <h1>{this.state.nome}</h1>
+        <h2>{this.state.idade}</h2>
+        <h3>{this.state.Sua_comida_favorita}</h3>
+        <ul>
+          <li>{this.state.musica[0]}</li>
+          <li>{this.state.musica[1]}</li>
+          <li>{this.state.musica[2]}</li>          
+        </ul>
+        <img src={fruta}/>
       </header>
-    </div>
-  );
+
+     ) 
+  } 
 }
 
-export default App;
